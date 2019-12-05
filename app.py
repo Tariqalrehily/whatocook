60592564944b584ff64b2143a94971f900ae5c7a
Tariqalrehily <tariqalrehily@gmail.com>
2019-12-05 20:16:45 +0000

    connect mongoDB to the App, also adding add temlates folder which has recipes.html with basic html code to test mongoDB connection to the App

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "what_to_cook"
+app.config["MONGO_URI"] = "mongodb+srv://root:Anatar1010@myfirstcluster-ddwj9.mongodb.net/what_to_cook?retryWrites=true&w=majority"
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
-
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
