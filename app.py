8b35b3cfe9c2b21e0f3f13f6b6957ee652139778
Tariqalrehily <tariqalrehily@gmail.com>
2019-12-09 16:00:11 +0000

    Added templates/editrecipe.html and static directory. Modify app.py to include the new route for edit and update the recipe. Add style.css with some elements styling. Also, modify base.html to have container class for block content, comment recommended switch button for now

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "whatocook"
@@ -16,7 +17,32 @@ def get_recipes():
 def add_recipe():
     return render_template('addrecipe.html', add_recipe=mongo.db.cuisine.find());
 
+# insert recipe route, insert as dictionary, so it can easily be understood by Mongo
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
 
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id: ObjectId(recipe_id)"})
+    all_cuisines = mongo.db.cuisine.find()
+    return render_template('editrecipe.html', recipe=the_recipe, cuisine=all_cuisines)
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipe = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_cuisine':request.form.get('recipe_cuisine'),
+        'recipe_food_type':request.form.get('recipe_food_type'),
+        'recipe_name':request.form.get('recipe_name'),
+        'ingredients': request.form.get('ingredients'),
+        'preparation_steps': request.form.get('preparation_steps'),
+        'required_tools':request.form.get('required_tools')
+    })
+    return redirect(url_for('get_recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
