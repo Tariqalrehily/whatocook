9c3eeea9010eec973e664de8c805b4013f6f254a
Tariqalrehily <Tariqalrehily@gmail.com>
2019-12-30 00:10:33 +0000

    Checked app.py code for PEP8 requirements

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app.config["MONGO_DBNAME"] = "whatocook"
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
+
 @app.route('/')
 # ----------------- recipes  -----------------
 # routes for users to find, edit, update and deleted recipes
-
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
@@ -24,35 +24,35 @@ def get_recipes():
 def add_recipe():
     return render_template('addrecipe.html', cuisines=mongo.db.cuisines.find())
 
-# insert recipe route, insert as dictionary, so it can easily be understood by mongo
+# insert recipe route as dictionary, so it can easily be understood by mongo
+
+
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    recipes =  mongo.db.recipes
+    recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
 
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
-    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    cuisines_types =  mongo.db.cuisines.find()
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    cuisines_types = mongo.db.cuisines.find()
     return render_template('editrecipe.html', recipe=the_recipe, cuisines=cuisines_types)
 
 
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
-    recipes.update( {'_id': ObjectId(recipe_id)},
-    {
-        'recipe_cuisine':request.form.get('recipe_cuisine'),
-        'recipe_food_type':request.form.get('recipe_food_type').upper(),
-        'recipe_name':request.form.get('recipe_name'),
+    recipes.update({'_id': ObjectId(recipe_id)}, {  
+        'recipe_cuisine': request.form.get('recipe_cuisine'),
+        'recipe_food_type': request.form.get('recipe_food_type').upper(),
+        'recipe_name': request.form.get('recipe_name'),
         'ingredients': request.form.get('ingredients'),
         'preparation_steps': request.form.get('preparation_steps'),
-        'required_tools':request.form.get('required_tools'),
-        'image':request.form.get('image')
-    })
-    
+        'required_tools': request.form.get('required_tools'),
+        'image': request.form.get('image')
+    })  
     return redirect(url_for('get_recipes'))
 
 
@@ -62,6 +62,7 @@ def delete_recipe(recipe_id):
     return redirect(url_for('get_recipes'))
 
 # ----------------- cuisine  -----------------
+
 
 @app.route('/get_cuisines')
 def get_cuisines():
@@ -98,8 +99,10 @@ def insert_cuisine():
 @app.route('/add_cuisine')
 def add_cuisine():
     return render_template('addcuisine.html')
-    
+
+
 # ----------------- search by cuisine  -----------------
+
 
 @app.route('/search_by_cuisine')
 def search_by_cuisine():
@@ -110,23 +113,24 @@ def search_by_cuisine():
 def recipes_by_cuisine():
     recipe = mongo.db.recipes.find()
     search = request.form.get('recipe_cuisine')
-    #print out the search variable - see what we are getting back from the form
+# print out the search variable - see what we are getting back from the form
     print(search)
     search_cuisine = mongo.db.recipes.find({"recipe_cuisine": (search)})
-    #print out search_cuisine - can see if we're getting a response from mongodb
+# print out search_cuisine - can see if we're getting a response from mongodb
     print(search_cuisine)
     return render_template("recipesbycuisine.html", recipe=recipe, search_cuisine=search_cuisine)
 
 # ----------------- search by recipe  -----------------
 
+
 @app.route('/recipes_by_name', methods=["POST"])
 def recipes_by_name():
     recipe = mongo.db.recipes.find()
     search = request.form.get('recipe_name')
-    #print out the search variable - see what we are getting back from the form
+# print out the search variable - see what we are getting back from the form
     print(search)
     search_names = mongo.db.recipes.find({"recipe_name": (search)})
-    #print out search_cuisine - can see if we're getting a response from mongodb
+# print out search_cuisine - can see if we're getting a response from mongodb
     print(search_names)
     return render_template("recipesbyname.html", recipe=recipe, search_names=search_names)
 
@@ -134,4 +138,4 @@ def recipes_by_name():
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
             port=(os.environ.get('PORT')),
-            debug=True)
+            debug=True)        
